eff7d0ff2029237154efe297ea6dfd78c86eaf82
Anatoly Alexeev <olorin@mail.ru>
2026-05-14 02:47:50 +0300

    fix(training): add missing os import in utils.py

--- training/utils.py
+++ training/utils.py
@@ -14,6 +14,7 @@ from __future__ import annotations
 # -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 import logging
+import os
 import re
 import sys
 from textwrap import shorten
