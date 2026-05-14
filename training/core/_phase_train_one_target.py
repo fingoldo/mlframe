110b4c520cf3ad668822f4802d086dbe9e911dc9
Anatoly Alexeev <olorin@mail.ru>
2026-05-14 03:00:02 +0300

    fix: run_dummy_baselines import in _phase_train_one_target

--- training/core/_phase_train_one_target.py
+++ training/core/_phase_train_one_target.py
@@ -22,6 +22,7 @@ from ..phases import phase
 from ..utils import log_ram_usage
 from ._misc_helpers import _elapsed_str
 from ._phase_diagnostics import run_per_target_diagnostics
+from ._phase_dummy_baselines import run_dummy_baselines
 from ._setup_helpers import _setup_model_directories
 from ..strategies import PipelineCache
 
